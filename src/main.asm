265c6b0efd1a8d84cb0740a731296bb429de2954
Mediador09 <fer.amdias@gmail.com>
2025-06-26 13:38:41 -0300

    feat: atualizar lógica de impressão de fase e organização do código no procedimento MAIN

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,8 @@
 .data
-POSICOES_MAPA: .byte 0, 0
-POSICAO_PLAYER: .byte 0, 0
-FASE_ATUAL: .byte 0
+
+.include ".\memoria.asm"
+.include "..\example.data"
+.include "..\Texturas\placeholder.data"
 
 .text
 
@@ -13,13 +14,22 @@ MAIN:
 		li a1, 0			# frame 0
 		jal PROC_PREENCHER_TELA		# preenche a tela de preto
 		
+		# PROC_IMPRIMIR_FASE tem dois argumentos: 
+		#	a0 (endereco do mapa);
+		#	a1 (endereco da textura); 
+		
+		la a0, example
+		la a1, placeholder
+		jal PROC_IMPRIMIR_FASE
+		
 		# O QUE PRECISA SER COLOCADO AQUI:
 		#	 INTRO DO JOGO
 		#	 LOOP DO MENU ( E SELECAO DE OPCOES )
 		#	 LOOP MAIOR DO JOGO:
-		#		IMPRESSAO DO MAPA
-		#		POSICIONAMENTO DE INIMIGOS (UTILIZANDO UM VETOR NA MEMORIA PARA GUARDAR TODOS ELES)
-		#		POSICIONAMENTO DO JOGADOR
+# FEITO				IMPRESSAO DO MAPA		
+# FEITO				POSICIONAMENTO DE INIMIGOS (UTILIZANDO UM VETOR NA MEMORIA PARA GUARDAR TODOS ELES)
+# FEITO				POSICIONAMENTO DO JOGADOR  (UTILIZANDO POSICAO_PLAYER)
+		#		IMPRESSAO DO INIMIGO E DO JOGADOR
 		#		LOOP MENOR DO JOGO:
 		#			REGISTRAR MOVIMENTO DE JOGADOR
 		#			DESIMPRIMIR JOGADOR (E O(S) TILE(S) QUE ELE ESTAVA)
@@ -35,9 +45,15 @@ MAIN:
 		#			DESIMPRIMIR OS TILES EM QUE OS INIMIGOS ESTAVAM
 		#			REIMPRIMIR OS TILES EM QUE OS INIMIGOS ESTAVAM
 		#			CALCULAR SE ALGUM INIMIGO TOCOU NO JOGADOR
+		#
+		#			TOCAR AUDIO
+		#			TOCAR MUSICA
 			
 		
-FIM:		li a7, 10			# syscall pra terminar o programa
+FIM:		
+
+
+		li a7, 10			# syscall pra terminar o programa
 		ecall				# estritamente necessario pra impedir que o programa continue ate entrar no codigo dentro dos includes. 
 		
 
