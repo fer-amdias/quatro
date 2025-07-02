98ffc2d698f15ed178d290b55ccf6f6ac78c2acb
Mediador09 <fer.amdias@gmail.com>
2025-07-01 21:37:23 -0300

    feat: adicionar macro de impressão de inteiros

--- src/macros.asm
+++ src/macros.asm
@@ -1,7 +1,7 @@
 #################################################
 #		REGISTRO DE MACROS		#
 #################################################
-# Aqui define-se as macros ah serem utilizadas   #
+# Aqui define-se as macros a serem utilizadas   #
 # no jogo.					#
 # 						#
 # Como esse arquivo estah incluido em 		#
@@ -10,7 +10,13 @@
 #################################################
 
 .macro print (%string_address)
-li a7, 4
-la a0, %string_address
-ecall
+	li a7, 4
+	la a0, %string_address
+	ecall
+.end_macro
+
+.macro print_int (%int)
+	li a7, 1
+	mv a0, %int
+	ecall
 .end_macro
