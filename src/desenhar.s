a3f0be602ece32e11e63abe169ccc292c13ccb85
Mediador09 <fer.amdias@gmail.com>
2025-07-18 23:32:20 -0300

    refactor: remover instrução inutilizada

--- src/desenhar.s
+++ src/desenhar.s
@@ -13,7 +13,6 @@
 PROC_DESENHAR:		lw t1, FRAME_BUFFER_PTR		# carrega o endereco do frame buffer
 			lw t2, FRAME_BUFFER_FIM_PTR	# endereco final 
 			li t3, FRAME_0			# 0xFF000000 - endereco inicial
-			li t4, FRAME_0_FIM		# 0xFF012C00 - endereco final
 	
 P_D1_LOOP: 		beq t1,t2,P_D1_FIM		# Se for o ulltimo endereco entao sai do loop
 			lw t0, (t1)			# le uam word no buffer
