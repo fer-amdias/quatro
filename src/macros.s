8e0a546d1f668000d57b548b0f4e17d046644040
Mediador09 <fer.amdias@gmail.com>
2025-07-22 08:52:09 -0300

    feat(macros): adicionar macro de normalizar posição.

--- src/macros.s
+++ src/macros.s
@@ -93,4 +93,20 @@ MACRO_DATA_QUEBRA_DE_LINHA: .string "\n"
 	ecall
 .end_macro
 
+# rg x, rg y
+.macro normalizar_posicao(%x, %y)
+	la t0, POSICOES_MAPA          # t0 -> word com X e Y do mapa
+	lhu t1, 0(t0)                 # t1 = POSICAO_MAPA_X
+	lhu t2, 2(t0)                 # t2 = POSICAO_MAPA_Y
+	
+	li  t0, TAMANHO_SPRITE
+
+	sub t3, %x, t1                # t3 = X relativo ao mapa
+	rem t4, t3, t0                # t4 = X % TAMANHO_SPRITE
+	sub %x, %x, t4                # x -= resto
+
+	sub t3, %y, t2                # t3 = Y relativo ao mapa
+	rem t4, t3, t0                # t4 = Y % TAMANHO_SPRITE
+	sub %y, %y, t4                # y -= resto
+.end_macro
 
