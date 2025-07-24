0f68d4e1bdc5f4e12b3a57a7b37b730515b770cb
Mediador09 <fer.amdias@gmail.com>
2025-07-23 22:48:14 -0300

    feat: adicionar maximo de vidas (4)

--- src/imprimir_fase.s
+++ src/imprimir_fase.s
@@ -320,9 +320,11 @@ P_IF1_FIM:			# traz os registradores salvos de volta da stack
 				# adiciona uma vida
 				la t0, VIDAS_RESTANTES
 				lbu t1, (t0)
+				li t2, MAX_VIDAS
+				bge t1, t2, P_IF1_MAX_VIDAS	# se vidas >= MAX_VIDAS, nao incrementa (eh o maximo
 				addi t1, t1, 1
 				sb t1, (t0)
-
+P_IF1_MAX_VIDAS:
 
 				ret
 					
