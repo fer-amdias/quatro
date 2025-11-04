9b6e636b9a543719d600f55149044dabea941eec
Mediador09 <fer.amdias@gmail.com>
2025-11-04 11:51:28 -0300

    feat: adicionar campo de tipo de movimento a struct de definicao de NPCs

--- src/NPCs/definicoes_npcs.s
+++ src/NPCs/definicoes_npcs.s
@@ -17,25 +17,30 @@
 .eqv NAO_EH_INIMIGO     0
 .eqv EH_INIMIGO         1
 
-.eqv NPC_STRUCT_TAMANHO 2       
+.eqv NPC_STRUCT_TAMANHO 3       
 
 STRUCT_NPCS:
         # NPC 1 - Seta vermelha
-        .byte 100                # VELOCIDADE
+        .byte 100               # VELOCIDADE
         .byte EH_INIMIGO        # INIMIGO?
+        .byte 1                 # TIPO DE MOVIMENTO (QUAL PROC CHAMAR)    
         
         # NPC 2 - Sentinelas Babilonicas
-        .byte 100                # VELOCIDADE
+        .byte 100               # VELOCIDADE
         .byte EH_INIMIGO        # INIMIGO?
+        .byte 1                 # TIPO DE MOVIMENTO  
 
         # NPC 3 - Ratos
-        .byte 160                # VELOCIDADE
+        .byte 160               # VELOCIDADE
         .byte NAO_EH_INIMIGO    # INIMIGO?
+        .byte 2                 # TIPO DE MOVIMENTO
 
         # NPC 4 - Legionarios Romanos
         .byte 60                # VELOCIDADE
         .byte EH_INIMIGO        # INIMIGO?
+        .byte 3                 # TIPO DE MOVIMENTO
 
         # NPC 5 - Filosofos
-        .byte 80                 # VELOCIDADE
+        .byte 80                # VELOCIDADE
         .byte NAO_EH_INIMIGO    # INIMIGO?
+        .byte 4                 # TIPO DE MOVIMENTO
