33839eafa85ce456e9b9edb0dca4758013d425e8
Mediador09 <fer.amdias@gmail.com>
2026-01-10 23:12:41 -0300

    feat: atualizar menu de carregar
    reposicionar elemos de UI
    acabar com possibilidade de buffer overflow
    adicionar aviso de arquivo nao encontrado

--- src/editor_de_fases/menu_carregar.s
+++ src/editor_de_fases/menu_carregar.s
@@ -5,9 +5,8 @@
 .data
 
 MENU_CARREGAR_STR_TERMINAL: .asciz "assets/fases/"
-MENU_CARREGAR_STR_PATH: .asciz "../assets/fases"
-MENU_CARREGAR_STR_BUFF: .space 64     # buffer para o nome do arquivo
 MENU_CARREGAR_STR_CARACTERES_DIGITADOS: .byte 0
+MENU_CARREGAR_ARQUIVO_NAO_ENCONTRADO: .byte 0
 
 .text
 
@@ -16,12 +15,14 @@ EDITOR_MENU_CARREGAR:
 	addi sp, sp, -4
 	sw ra, (sp)
 
-	la t0, MENU_CARREGAR_STR_PATH
+	la t0, ARQUIVO_STR_PATH
 	li t1, '/'
-	sb t1, 15(t0)	# correcao de erro: substitui o null-terminated character de MENU_CARREGAR_STR_PATH por uma barra.
+	sb t1, 15(t0)	# correcao de erro: substitui o null-terminated character de ARQUIVO_STR_PATH por uma barra.
 	# isso eh estritamente necessario: na versao 1.13 do FPGRARS, nao tem como declarar uma non-null-terminated string.
 	# portanto, devemos destruir o \0 substituindo-o no codigo pelo que queremos no lugar, fazendo uma non-null-terminated
 	# string na pratica.
+
+	sb zero, MENU_CARREGAR_ARQUIVO_NAO_ENCONTRADO, t0 # reseta o estado da flag
 
 E_MC2_MENU:
 
@@ -39,19 +40,26 @@ E_MC2_MENU:
 	jal PROC_IMPRIMIR_TEXTURA
 
         imprimir_string(EDITOR_MENU_CARREGAR_PROMPT, 100, 134, 0xC7FF, 0)
-        imprimir_string(MENU_CARREGAR_STR_TERMINAL, 10, 154, 0xC7FF, 1)
-        imprimir_string(MENU_CARREGAR_STR_BUFF, 114, 154, 0xC7FF, 1)
-        imprimir_string(EDITOR_MENU_OPCAO_9, 100, 169, 0xC7FF, 0)
+        imprimir_string(MENU_CARREGAR_STR_TERMINAL, 10, 149, 0xC7FF, 1)
+        imprimir_string(STR_NOME_ARQUIVO, 114, 149, 0xC7FF, 1)
+        imprimir_string(EDITOR_MENU_OPCAO_9, 100, 164, 0xC7FF, 0)
+	# TODO: use ESC instead of 9 in case user file contains the letter 9 lmao
+	# why didnt i foresee this problem earlier
 
-        jal PROC_DESENHAR
+	lb t0, MENU_CARREGAR_ARQUIVO_NAO_ENCONTRADO
+	beqz t0, E_MC2_MENU_CONT
+
+	# se o arquivo nao foi encontrado
+
+	imprimir_string(EDITOR_MENU_CARREGAR_NAO_ENCONTRADO, 100, 189, 0xC737, 0)
+	
+E_MC2_MENU_CONT:
+	jal PROC_DESENHAR
 
 E_MC2_LOOP:
 
 	li a0, 0
-        lb t0, MUTADO
-        bnez t0, E_MC2_LOOP_SEM_AUDIO
         jal PROC_TOCAR_AUDIO	
-E_MC2_LOOP_SEM_AUDIO:
 
 	li t1,0xFF200000		# carrega o endereco de controle do KDMMIO
 	lw t0,0(t1)			# Le bit de Controle Teclado
@@ -67,8 +75,12 @@ E_MC2_LOOP_SEM_AUDIO:
 	beq t2, t0, E_MC2_ENTER
 
 E_MC2_ADD_CARACTERE:
-	la t0, MENU_CARREGAR_STR_BUFF
+	la t0, STR_NOME_ARQUIVO
 	lbu t1, MENU_CARREGAR_STR_CARACTERES_DIGITADOS
+
+	li t3, 255
+	bge t1, t3, E_MC2_MENU	# cancela se chegamos no final do buffer
+
 	add t0, t0, t1	# vai ate o caractere a ser digitado
 
 	sb t2, (t0)	# adiciona o caractere
@@ -81,7 +93,7 @@ E_MC2_ADD_CARACTERE:
         j E_MC2_MENU
 
 E_MC2_REMOVE_CARACTERE:
-	la t0, MENU_CARREGAR_STR_BUFF
+	la t0, STR_NOME_ARQUIVO
 	lbu t1, MENU_CARREGAR_STR_CARACTERES_DIGITADOS
 	beqz t1, E_MC2_MENU	# nao tira nada se nao tiver caracteres
 	addi t1, t1, -1		# volta um caractere
@@ -93,23 +105,35 @@ E_MC2_REMOVE_CARACTERE:
 
 E_MC2_VOLTAR:
 	sb zero, MENU_CARREGAR_STR_CARACTERES_DIGITADOS, t0
-	sb zero, MENU_CARREGAR_STR_BUFF, t0
+	sb zero, STR_NOME_ARQUIVO, t0
 	lw ra, (sp)
 	addi sp, sp, 4
 	ret
 
 E_MC2_ENTER:
-	la a0, MENU_CARREGAR_STR_PATH
+	la a0, ARQUIVO_STR_PATH
 	li a1, 0	# read-only
 	li a7, 1024
 	ecall
-	safe_print_int_ln(a0)
 
-	la a0, MENU_CARREGAR_STR_PATH
-	li a7, 4
-	ecall
+	bgtz a0, E_MC2_CARREGAR_FASE
+	# se a0 < 0, entao nao foi possivel abrir a fase!
 
-	quebra_de_linha
+	li t0, 1
+	sb t0, MENU_CARREGAR_ARQUIVO_NAO_ENCONTRADO, t1
+
+	# reseta a string
+	sb zero, MENU_CARREGAR_STR_CARACTERES_DIGITADOS, t0
+	sb zero, STR_NOME_ARQUIVO, t0
+
 	j E_MC2_MENU
+
+E_MC2_CARREGAR_FASE:
+
+	# jal EDITOR_CARREGAR_FASE
+
+	li a7, 10
+	ecall
+	# finaliza, placeholder
 
 
