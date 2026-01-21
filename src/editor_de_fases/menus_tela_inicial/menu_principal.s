daa56ae8298b6cfb18be9a17db794fcad9a38328
Mediador09 <fer.amdias@gmail.com>
2026-01-21 15:38:50 -0300

    perf: aumentar tempo de idle por ciclo do menu principal

--- src/editor_de_fases/menus_tela_inicial/menu_principal.s
+++ src/editor_de_fases/menus_tela_inicial/menu_principal.s
@@ -40,7 +40,7 @@ E_MP1_LOOP:
 	li a0, 0
         jal PROC_TOCAR_AUDIO	
 
-	sleep(4) # performance
+	sleep(10) # performance
 
 	li t1,0xFF200000		# carrega o endereco de controle do KDMMIO
 	lw t0,0(t1)			# Le bit de Controle Teclado
