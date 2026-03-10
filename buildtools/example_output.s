74c111df4eb26237ae89bd90cfb67bdda7d49aa4
Mediador09 <fer.amdias@gmail.com>
2026-03-10 09:01:25 -0300

    chore: atualizar example_output

--- buildtools/example_output.s
+++ buildtools/example_output.s
@@ -1,23 +1,24 @@
-.eqv PT-BR 0
-.eqv EN-US 1
-.eqv DE-DE 2
-.eqv ELFNT 3
+.eqv tags 0
+.eqv PT-BR 1
+.eqv EN-US 2
+.eqv DE-DE 3
+.eqv ELFNT 4
 
 
 .data
 
-qtd_de_linguas: .byte 4
+qtd_de_linguas: .byte 5
 lingua_atual:   .byte PT-BR
 
 offset: 
-tags:                                    .word        0,       73,      140,      212, 
-lang_name:                               .word        6,       79,      146,      218, 
-example:                                 .word       27,       92,      159,      227, 
-ex2:                                     .word       44,      111,      179,      261, 
-exit:                                    .word       52,      119,      188,      312, 
-teste:                                   .word       57,      124,      196,      350, 
-delim:                                   .word       63,      130,      202,      356, 
+tags:                                    .word        0,       44,      117,      184,      256, 
+lang_name:                               .word        5,       50,      123,      190,      262, 
+example:                                 .word       15,       71,      136,      203,      271, 
+ex2:                                     .word       23,       88,      155,      223,      305, 
+exit:                                    .word       27,       96,      163,      232,      356, 
+teste:                                   .word       32,      101,      168,      240,      394, 
+delim:                                   .word       38,      107,      174,      246,      400, 
 
-strblock: .asciz "PT-BR" "Portugues Brasileiro" "Bom dia, senhor." "exemplo" "sair" "12345" "fim_PT-BR" "EN-US" "English (US)" "Good morning, sir." "example" "exit" "67890" "fim_EN-US" "DE-DE" "Deutsch (DE)" "Guten morgen, Herr." "Beispiel" "beenden" "ABCDE" "fim_DE-DE" "ELFNT" "Elefante" "Um elefante incomoda muita gente," "Dois elefantes\n- incomodam\n- incomodam\nmuito mais!" "Tres elefantes incomodam muita gente," "FGHIJ" "fim_ELFNT" "" 
+strblock: .asciz "tags" "lang_name" "example" "ex2" "exit" "teste" "delim" "PT-BR" "Portugues Brasileiro" "Bom dia, senhor." "exemplo" "sair" "12345" "fim_PT-BR" "EN-US" "English (US)" "Good morning, sir." "example" "exit" "67890" "fim_EN-US" "DE-DE" "Deutsch (DE)" "Guten morgen, Herr." "Beispiel" "beenden" "ABCDE" "fim_DE-DE" "ELFNT" "Elefante" "Um elefante incomoda muita gente," "Dois elefantes\n- incomodam\n- incomodam\nmuito mais!" "Tres elefantes incomodam muita gente," "FGHIJ" "fim_ELFNT" "" 
 
-# VERSAO DA BUILDTOOL CSV2ASM: 1.2.0 - 05 outubro 2025
+# VERSAO DA BUILDTOOL CSV2ASM: 1.4.0 - 18 fevereiro 2026
