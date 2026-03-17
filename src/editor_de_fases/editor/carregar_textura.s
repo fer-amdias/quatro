f7efbf5d32553897ebcf5b2c9657aa6c94d6c6ae
Mediador09 <fer.amdias@gmail.com>
2026-03-17 17:35:30 -0300

    fix: carregar textura para o buffer argumento corretamente

--- src/editor_de_fases/editor/carregar_textura.s
+++ src/editor_de_fases/editor/carregar_textura.s
@@ -1,18 +1,25 @@
 # EDITOR_CARREGAR_TEXTURA				       	
-# Carrega uma textura de um binario para o buffer de fase
+# Carrega uma textura de um binario para o buffer fornecido
 # ARGUMENTOS:						     	
 #	A0 : string com o nome da textura    
 #       A1 : buffer para colocar textura                      
 # RETORNOS:                                                  
 #       A0 : quantidade de bytes lidos. -1 se houve uma falha.
 
-EDITOR_CARREGAR_TEXTURA:
+.data
 
+CT1_BUFFER_DIMENSOES: .space 8
+
+.text
+
+EDITOR_CARREGAR_TEXTURA:
         addi sp, sp, -8
         sw s0, (sp) # file descriptor
         sw s1, 4(sp) # buffer destino
 
 E_CT1_ABRIR_ARQUIVO:
+        mv s1, a1       # guarda o buffer destino
+
 	# a0 carregado
 	li a1, 0	# read-only
 	li a7, 1024     # abrir arquivo
@@ -24,10 +31,11 @@ E_CT1_ABRIR_ARQUIVO:
         mv s0, a0       # guarda o descritor de arquivo em s0
 
         # s0 = descritor de arquivo
+        # s1 = endereco do buffer destino
 
 E_CT1_CALCULAR_TAMANHO:
         # a0 carregado
-        la a1, TEXTURA_BUFFER
+        la a1, CT1_BUFFER_DIMENSOES
         li a2, 8        # vamos ler as duas primeiras words
         li a7, 63       # ler
         ecall
@@ -38,7 +46,7 @@ E_CT1_CALCULAR_TAMANHO:
         blt a0, t0, E_CT1_FALHA # se nao conseguimos ler OITO BYTES, nao tem esperanca. 
 
         # pega as dimensoes da textura
-        la t0, TEXTURA_BUFFER
+        la t0, CT1_BUFFER_DIMENSOES
         lw t1, (t0)
         lw t2, 4(t0)
 
@@ -50,18 +58,22 @@ E_CT1_CALCULAR_TAMANHO:
 
         # falha se overflow
         bltz t0, E_CT1_FALHA
+
+        # coloca as dimensoes no buffer
+        sw t1, (s1)
+        sw t2, 4(s1)
+        addi s1, s1, 8
         
 
 E_CT1_LER_ARQUIVO:
         # agora lemos tudo de uma vez
         mv a0, s0               # file descriptor
         mv a1, s1               # buffer de textura
-        addi a1, a1, 8          # pula os bytes que jah lemos
         mv a2, t0               # quantidade
         li a7, 63               # LER
         ecall
 
-E_CT1_RET:
+
         mv t0, a0               # guarda temporariamente a qtd de bytes lidos em t0
 
         mv a0, s0               # coloca
@@ -70,6 +82,7 @@ E_CT1_RET:
 
         mv a0, t0               # retorna quantos bytes foram lidos
 
+E_CT1_RET:
         lw s0, (sp)
         lw s1, 4(sp)
         addi sp, sp, 8
