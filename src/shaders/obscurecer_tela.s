20a4177dcc77e23437dc96a4bb2baff487ac9fc6
Mediador09 <fer.amdias@gmail.com>
2026-03-17 11:09:24 -0300

    refactor: atualizar shader de obscurecer tela
    Fazer com que procedimento PRIMEIRO copie o conteudo da tela para o frame_buffer, DEPOIS aplique o shader em cima do frame_buffer

--- src/shaders/obscurecer_tela.s
+++ src/shaders/obscurecer_tela.s
@@ -16,13 +16,27 @@ SHADER_OBSCURECER_TELA:
         bgt t1, t0, S_OT1_FRAME_0
         li t1, FRAME_1
         li t2, FRAME_1_FIM
-        j S_OT1_MAIN		
+        li t3, FRAME_0
+        j S_OT1_COPIAR		
 
 S_OT1_FRAME_0:
         li t1, FRAME_0
         li t2, FRAME_0_FIM
+        li t3, FRAME_1
+
+S_OT1_COPIAR:
+        # vamos copiar tudo da tela atual para o frame_buffer
+        beq t1,t2,S_OT1_MAIN		# se for o ultimo endereco ent sai do loop
+        lw t4,0(t1)                     # pega uma word da tela
+        sw t4,0(t3)			# salva a word no buffer
+        addi t3,t3,4			# pula uma word
+        addi t1,t1,4                    # pula uma word
+        j S_OT1_COPIAR			# volta a verificar
+
 S_OT1_MAIN: 		
         mv t0, zero                     # marcador = 0
+        lw t1, FRAME_BUFFER_PTR
+        lw t2, FRAME_BUFFER_FIM_PTR
         mv t3, t1                       # usaremos o t3 como pointer de endereco a ser incrementado
         mv t4, zero                     # X = 0
         li t6, LARGURA_VGA
@@ -39,7 +53,7 @@ S_OT1_PROXIMA_LINHA:
         mv t4, zero                     # reseta o X
         seqz t0, t0                      # inverte o marcador
         # SE P >= Pm sai do loop
-        bge t3, t2, S_OT1_CONT
+        bge t3, t2, S_OT1_FIM
         
 S_OT1_LOOP:		
         beqz t0, S_OT1_PULA_PIXEL	# SE ( marcador = falso ), ENTAO PULA O PIXEL
@@ -55,18 +69,6 @@ S_OT1_PULA_PIXEL:
         beq t4, t6, S_OT1_PROXIMA_LINHA
 
         j S_OT1_LOOP			# continua o loop		
-
-S_OT1_CONT:
-        lw t3, FRAME_BUFFER_PTR		# carrega o endereco do frame buffer
-
-S_OT1_COPIAR_PARA_FRAME_BUFFER:
-        # agora devemos pegar o que estah em t1 e colocar em FRAME_BUFFER
-        beq t1,t2,S_OT1_FIM		# se for o ultimo endereco ent sai do loop
-        lw t4,0(t1)                     # pega uma word da tela
-        sw t4,0(t3)			# salva a word no buffer
-        addi t3,t3,4			# pula uma word
-        addi t1,t1,4                    # pula uma word
-        j S_OT1_COPIAR_PARA_FRAME_BUFFER			# volta a verificar
 
 S_OT1_FIM:
         ret
