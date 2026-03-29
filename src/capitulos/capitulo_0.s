4ca8b2f437e5223a5e30d38c36211856888da4dc
Mediador09 <fer.amdias@gmail.com>
2026-03-29 20:45:08 -0300

    feat: atualizar arquivos de capítulo 0 e 1

--- src/capitulos/capitulo_0.s
+++ src/capitulos/capitulo_0.s
@@ -17,6 +17,7 @@ CAPITULO_0:
 
 .data
 
+ch0_fase0: .string "../assets/fases/ch0_fase0.lvl"
 ch0_fase1: .string "../assets/fases/ch0_fase1.lvl"
 ch0_fase2: .string "../assets/fases/ch0_fase2.lvl"
 ch0_fase3: .string "../assets/fases/ch0_fase3.lvl"
@@ -24,8 +25,6 @@ ch0_fase4: .string "../assets/fases/ch0_fase4.lvl"
 ch0_fase5: .string "../assets/fases/ch0_fase5.lvl"
 ch0_fase6: .string "../assets/fases/ch0_fase6.lvl"
 ch0_fase7: .string "../assets/fases/ch0_fase7.lvl"
-ch0_fase8: .string "../assets/fases/ch0_fase8.lvl"
-CAPITULO_0_TEXTURA: .string "../assets/texturas/ch0.bin"
 
 .text
 
@@ -33,45 +32,19 @@ CAPITULO_0_TEXTURA: .string "../assets/texturas/ch0.bin"
 ROTINA_CAPITULO_0:
 		addi sp, sp, -4
 		sw ra, (sp)
-		
+
 		li t0, 0
 		sw t0, VIDAS_RESTANTES, t1		# reseta as vidas restantes para 0
-		#############
-
-		# carrega a textura
-
-		la a0, CAPITULO_0_TEXTURA
-		la a1, TEXTURA_DO_MAPA_BUFFER
-		jal PROC_CARREGAR_TEXTURA
 		
-		# int fase, int capitulo, label mapa, int tempo_limite, 
-		# label textura_inimigos, label textura_jogador
-		# label musica, label sfx_powerup, lavel sfx_morte, label sfx_scroll, 
-		# label mapa.scroll, bool mostrar_scroll_no_inicio, label textura_do_pergaminho
-		# bool modo_saida_livre, label textura_de_fundo
-		FASE(1, 0, ch0_fase1, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase1.scroll, pergaminho, 1, 1, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(2, 0, ch0_fase2, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase2.scroll, pergaminho, 1, 1, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(3, 0, ch0_fase3, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase3.scroll, pergaminho, 1, 1, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(4, 0, ch0_fase4, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase4.scroll, pergaminho, 1, 1, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(5, 0, ch0_fase5, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase5.scroll, pergaminho, 0, 0, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(6, 0, ch0_fase6, TEXTURA_DO_MAPA_BUFFER, 100, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase6.scroll, pergaminho, 0, 0, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(7, 0, ch0_fase7, TEXTURA_DO_MAPA_BUFFER, 100, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase7.scroll, pergaminho, 0, 0, NULL)
-		beqz a0, C0_FIM
-		
-		FASE(8, 0, ch0_fase8, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, internationale, powerup, morte, abertura_pergaminho, ch0_fase8.scroll, pergaminho, 0, 0, NULL)
-		beqz a0, C0_FIM
+		# FASE(%fase, %capitulo, %nome_mapa, %label_de_morte)
+		FASE(0, 0, ch0_fase0, C0_FIM)
+		FASE(1, 0, ch0_fase1, C0_FIM)
+		FASE(2, 0, ch0_fase2, C0_FIM)
+		FASE(3, 0, ch0_fase3, C0_FIM)
+		FASE(4, 0, ch0_fase4, C0_FIM)
+		FASE(5, 0, ch0_fase5, C0_FIM)
+		FASE(6, 0, ch0_fase6, C0_FIM)
+		FASE(7, 0, ch0_fase7, C0_FIM)
 		
 C0_FIM:
 		lw ra, (sp)
--- src/capitulos/capitulo_1.s
+++ src/capitulos/capitulo_1.s
@@ -12,12 +12,11 @@ CAPITULO_1:
 
 .data
 
+ch1_fase0: .string "../assets/fases/ch1_fase0.lvl"
 ch1_fase1: .string "../assets/fases/ch1_fase1.lvl"
 ch1_fase2: .string "../assets/fases/ch1_fase2.lvl"
 ch1_fase3: .string "../assets/fases/ch1_fase3.lvl"
 ch1_fase4: .string "../assets/fases/ch1_fase4.lvl"
-ch1_fase5: .string "../assets/fases/ch1_fase5.lvl"
-CAPITULO_1_TEXTURA: .string "../assets/texturas/ch1.bin"
 
 .text
 
@@ -29,28 +28,18 @@ ROTINA_CAPITULO_1:
 		li t0, 0
 		sw t0, VIDAS_RESTANTES, t1			# reseta as vidas restantes para 0
 		
-		sh zero, POWERUP_TAMANHO_BOMBA, t0		# REESCREVE OS DOIS POWERUPS PARA 0
-		#############
+		# REESCREVE OS DOIS POWERUPS PARA 0
+		sb zero, POWERUP_TAMANHO_BOMBA, t0		
+		sb zero, POWERUP_QTD_BOMBAS, t0
 
-		la a0, CAPITULO_1_TEXTURA
-		la a1, TEXTURA_DO_MAPA_BUFFER
-		jal PROC_CARREGAR_TEXTURA
-		
-		#FASE(%fase, %capitulo, %arquivo_mapa, %textura_do_mapa, %tempo_limite, %textura_dos_inimigos, %textura_do_jogador, %musica_de_fundo, %audioefeito_de_powerup, %audioefeito_de_morte, %audioefeito_de_pergaminho, %endereco_pergaminho, %textura_do_pergaminho, %mostrar_pergaminho_no_inicio, %modo_saida_livre, %textura_de_fundo)
-		FASE(1, 1, ch1_fase1, TEXTURA_DO_MAPA_BUFFER, 70, inimigos, jogador, west__hurrian_song, powerup_ch3, morte_ch3, abertura_scroll_ch3, ch1_fase1.scroll, pergaminho_ch1, 0, 0, NULL)
-		beqz a0, C1_FIM
-		
-		FASE(2, 1, ch1_fase2, TEXTURA_DO_MAPA_BUFFER, 100, inimigos, jogador, west__hurrian_song, powerup_ch3, morte_ch3, abertura_scroll_ch3, ch1_fase2.scroll, pergaminho_ch1, 0, 0, NULL)
-		beqz a0, C1_FIM
-		
-		FASE(3, 1, ch1_fase3, TEXTURA_DO_MAPA_BUFFER, 120, inimigos, jogador, west__hurrian_song, powerup_ch3, morte_ch3, abertura_scroll_ch3, ch1_fase3.scroll, pergaminho_ch1, 0, 0, NULL)
-		beqz a0, C1_FIM
-		
-		FASE(4, 1, ch1_fase4, TEXTURA_DO_MAPA_BUFFER, 100, inimigos, jogador, west__hurrian_song, powerup_ch3, morte_ch3, abertura_scroll_ch3, ch1_fase4.scroll, pergaminho_ch1, 0, 0, NULL)
-		beqz a0, C1_FIM
-		
-		FASE(5, 1, ch1_fase5, TEXTURA_DO_MAPA_BUFFER, SEM_LIMITE_DE_TEMPO, inimigos, jogador, west__hurrian_song, powerup_ch3, morte_ch3, abertura_scroll_ch3, ch1_fase5.scroll, pergaminho_ch1, 0, 0, NULL)
-		beqz a0, C1_FIM
+		# FASE(%fase, %capitulo, %nome_mapa, %label_de_morte)
+		FASE(0, 1, ch1_fase0, C1_FIM)
+		FASE(1, 1, ch1_fase1, C1_FIM)
+		FASE(2, 1, ch1_fase2, C1_FIM)
+		FASE(3, 1, ch1_fase3, C1_FIM)
+		FASE(4, 1, ch1_fase4, C1_FIM)
+
+
 		
 		j C1_FIM
 		
